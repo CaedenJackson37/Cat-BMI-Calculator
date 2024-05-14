9014c521a2a300d9e3187b06ddef4fb6c9e1aa56
Caeden Jackson <150712960+CaedenJackson37@users.noreply.github.com>
2024-05-14 14:19:04 -0400

    Update Cat BMI Calculator.py

--- Cat BMI Calculator.py
+++ Cat BMI Calculator.py
@@ -4,10 +4,13 @@
 
 leg = int(input("Measure one of your cats rear legs in centimeters."))
 rib = int(input("Measure the ribcage of your cat around the ninth rib in centimeters."))
+
+# This is the equation to get the fbmi score
 fbmi = ((rib / 0.7062 - leg) / 0.9156) - leg
 print(fbmi)
 
 
+# This allows for telling if your cat if healthy or overweight based on their fbmi score
 if 15 <= fbmi <= 29.9:
     print("Your cat is at a normal weight.")
 elif 30 <= fbmi <= 42:
